98516583aa26841cbed68df0afac9c6fbdbd3f10
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-11-03 11:28:04 +0700

    Anh fix

--- src/Components/Host/MainCreateRoom/Third.jsx
+++ src/Components/Host/MainCreateRoom/Third.jsx
@@ -7,13 +7,13 @@ function Third() {
         <>
             <Navbar_create_room></Navbar_create_room>
             <div className='d-flex'>
-                <div className='col-5 ms-5 mt-5'>
+                <div className='col-6 ms-5 mt-5'>
                     <p className='fs-5 mb-5'>Bước 3</p>
                     <p className='fs-2 mb-5'>Hoàn thiện và đăng</p>
                     <p className='mb-5'>Cuối cùng, bạn sẽ chọn cài đặt đặt phòng, thiết lập giá và đăng mục cho thuê.</p>
                 </div>
-                <div>
-                    <img className='w-75 p-75' src="https://a0.muscache.com/4ea/air/v2/pictures/d7cb2e2b-f9b5-44e1-b510-44eaaf67889b.jpg" alt="" />
+                <div className='col-4'>
+                    <video autoPlay className='m-4 w-100' src='https://res.cloudinary.com/didieklbo/video/upload/f_auto:video,q_auto/v1/Gif/aewep3a0nq7mfcsbcnwg'></video>
                 </div>
             </div>
             <div className='fixed-bottom d-flex justify-content-between'>
